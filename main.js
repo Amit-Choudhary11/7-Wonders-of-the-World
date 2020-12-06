12f2e41d017d4000081576621df01ffb0dbc7578
Amit-Choudhary11 <74827051+Amit-Choudhary11@users.noreply.github.com>
2020-12-06 10:39:05 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function pg() {
+    window.alert("The Great Pyramid of Giza points very precisely to the north. The pyramids of Egypt are all built to the west of the Nile River. The base of a pyramid was always a perfect square. They were built mostly of limestone.");
+}
+
+function cr() {
+    window.alert("The Colossus of Rhodes was one of the Seven Wonders of the Ancient World. It represented the god Helios, and was built to thank the gods for the victory over Demetrius Poliorcetes' long siege (305 BCE) of Rhodes. The Colossus of Rhodes is familiar to almost everyone");
+}
+
+function l() {
+    window.alert("The Lighthouse of Alexandria was built on the island of Pharos outside the harbours of Alexandria, Egypt c. 300 – 280 BCE, during the reigns of Ptolemy I and II. With a height of over 100 metres (330 ft), it was so impressive that it made it to the established list of Seven Wonders of the Ancient World.");
+}
+
+function sz() {
+    window.alert("The Statue of Zeus at Olympia was about 42 feet tall. The statue was made of a wooden frame and covered in ivory and gold panels. The sculptor Phidias had previously created a similar sized statue of the goddess Athena. The sculpture of Athena was made for the Parthenon in Athens.");
+}
+
+function ta() {
+    window.alert("The Temple of Artemis at Ephesus located on the western coast of Asia Minor (modern Turkey) was built in the 6th century BCE, and such was its tremendous size, double the dimensions of other Greek temples including the Parthenon, that it was soon regarded as one of the Seven Wonders of the ancient world.");
+}
+
+function hgb() {
+    window.alert("King Nebuchadnezzar II ruled Babylon from 605BC, for a period of 43 years. It was during this time that he is said to have had the Hanging Gardens built. If the gardens actually existed, it would have taken 8,200 gallons of water each day to keep the plants watered. The gardens were thought to be about 75 feet high.");
+}
+
+function m() {
+    window.alert("This mausoleum was classified as a wonder because it distances itself by its beauty and its great value. Indeed, the ornaments of the sanctuary were realized by the most famous artists of its era: Satyrus and Pythius for the architecture; Scopas, Timotheos, Bryaxis and Leochares for the sculpture.");
+}
